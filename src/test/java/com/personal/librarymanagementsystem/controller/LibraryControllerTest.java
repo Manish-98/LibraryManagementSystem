5827ec7024797b1eb74a536aca11b74f5c7f80ac
Manish Kumar Lath <manishlath3445@gmail.com>
2023-01-13 00:54:20 +0530

    Pranali/Manish | Add controller test for request validations

--- src/test/java/com/personal/librarymanagementsystem/controller/LibraryControllerTest.java
+++ src/test/java/com/personal/librarymanagementsystem/controller/LibraryControllerTest.java
@@ -1,31 +1,131 @@
 package com.personal.librarymanagementsystem.controller;
 
 import com.personal.librarymanagementsystem.builder.AddressBuilder;
-import com.personal.librarymanagementsystem.controller.payload.LibraryCreationRequest;
+import com.personal.librarymanagementsystem.model.Address;
 import com.personal.librarymanagementsystem.model.Library;
 import com.personal.librarymanagementsystem.service.LibraryService;
-import org.junit.jupiter.api.Assertions;
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(LibraryController.class)
 class LibraryControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private LibraryService libraryService;
+
     @Test
-    void shouldSaveLibrary() {
-        LibraryService libraryService = mock(LibraryService.class);
-        LibraryController libraryController = new LibraryController(libraryService);
-        LibraryCreationRequest libraryCreationRequest = new LibraryCreationRequest("library", new AddressBuilder("100000").build());
-        Library expectedLibrary = new Library(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), "", new AddressBuilder("100000").build());
+    void shouldSaveLibrary() throws Exception {
+        @Language("JSON")
+        String requestBody = """
+                {
+                    "name": "Central Library",
+                    "address": {
+                        "zipCode": "100001"
+                    }
+                }
+                """.stripIndent();
 
-        when(libraryService.save(any())).thenReturn(expectedLibrary);
+        Address address = new AddressBuilder("100001").build();
+        Library centralLibrary = new Library(UUID.randomUUID(), "Central Library", address);
+        when(libraryService.save(any())).thenReturn(centralLibrary);
 
-        Library library = libraryController.save(libraryCreationRequest);
+        mockMvc.perform(post("/lms/api/v1/library")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(content().json(requestBody, false));
+    }
 
-        Assertions.assertEquals(expectedLibrary, library);
+    @Test
+    void shouldReturnBadRequestIfLibraryNameIsBlank() throws Exception {
+        @Language("JSON")
+        String requestBody = """
+                {
+                    "address": {
+                        "zipCode": "100001"
+                    }
+                }
+                """.stripIndent();
+
+        @Language("JSON")
+        String responseBody = """
+                {
+                    "name": ["Library name should not be blank"]
+                }
+                """;
+
+        mockMvc.perform(post("/lms/api/v1/library")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(responseBody, false));
+    }
+
+    @Test
+    void shouldReturnBadRequestIfAddressIsMissing() throws Exception {
+        @Language("JSON")
+        String requestBody = """
+                {
+                    "name": "Central Library"
+                }
+                """.stripIndent();
+
+        @Language("JSON")
+        String responseBody = """
+                {
+                    "address": ["Library address is missing"]
+                }
+                """;
+
+        mockMvc.perform(post("/lms/api/v1/library")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(responseBody, false));
+    }
+
+    @Test
+    void shouldReturnBadRequestIfAddressZipCodeIsInvalid() throws Exception {
+        @Language("JSON")
+        String requestBody = """
+                {
+                    "name": "Central Library",
+                    "address": {
+                        "zipCode": "someZipCode"
+                    }
+                }
+                """.stripIndent();
+
+        @Language("JSON")
+        String responseBody = """
+                {
+                    "address.zipCode": ["Invalid zip code"]
+                }
+                """;
+
+        mockMvc.perform(post("/lms/api/v1/library")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json(responseBody, false));
     }
 }
